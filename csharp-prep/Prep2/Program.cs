71afd2eb164661028677490d919271c56e01be11
Kryton Bess <krytonbess01@gmail.com>
2024-01-13 23:56:10 -0700

    Prep 1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string grade = "";
+
+        if (percent >= 90) {
+            grade = "A";
+        } else if (percent >= 80) {
+            grade = "B";
+        } else if (percent >= 70) {
+            grade = "C";
+        } else if (percent >= 60) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+
+        Console.WriteLine($"You got a {grade} in the class");
+        
+        if (percent >= 70) {
+            Console.WriteLine("Congrats, you passed!");
+        } else {
+            Console.WriteLine("Do better next time!");
+        }
     }
 }
